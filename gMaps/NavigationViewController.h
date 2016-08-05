81eb3de54bf830fa9e91e775f8fc2c59abc6397e
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-08-05 18:57:19 +0800

    ver 4.0

---
+++ gMaps/NavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationViewController.h
+//  gMaps
+//
+//  Created by LLDM on 05/08/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationViewController : UITableViewController
+
+@end
---
+++ gMaps/gMaps/FBViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBViewController.h
+//  gMaps
+//
+//  Created by LLDM on 05/08/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FBViewController : ViewController
+
+@end
